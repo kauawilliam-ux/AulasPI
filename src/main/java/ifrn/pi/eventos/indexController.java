2eda404cd250524099c68becc2db917009486641
William kaua Silva costa <william.kaua@academico.ifrn.edu.br>
2026-08-18 16:01:10 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/main/java/ifrn/pi/eventos/indexController.java
@@ -0,0 +1,14 @@
+package ifrn.pi.eventos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class indexController {
+	@RequestMapping("/")
+	public String index() {
+		System.err.println("Chamou o método index");
+		return "home";
+		
+	}
+}
